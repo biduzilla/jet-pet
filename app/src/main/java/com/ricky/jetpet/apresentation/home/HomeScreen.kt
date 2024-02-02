9fa19509a5dbc5d407625e6ec2d809ce0624216a
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2024-02-02 09:47:00 -0300

    topbar

--- app/src/main/java/com/ricky/jetpet/apresentation/home/HomeScreen.kt
+++ app/src/main/java/com/ricky/jetpet/apresentation/home/HomeScreen.kt
@@ -2,20 +2,35 @@ package com.ricky.jetpet.apresentation.home
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import com.ricky.jetpet.components.PetInfoItem
+import com.ricky.jetpet.components.TopBar
 import com.ricky.jetpet.data.DummyPetDataSource
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen() {
-    LazyColumn {
-        items(DummyPetDataSource.dogList) {pet->
-            PetInfoItem(pet = pet){
-
+fun HomeScreen(
+    onSwitchClick: () -> Unit,
+    onPetClick: (Int) -> Unit
+) {
+    val petList = DummyPetDataSource.dogList
+    Scaffold(
+        topBar = {
+            TopBar {
+                onSwitchClick()
+            }
+        }
+    ) { paddingValues ->
+        LazyColumn(contentPadding = paddingValues) {
+            itemsIndexed(petList) { i, pet ->
+                PetInfoItem(pet = pet) {
+                    onPetClick(i)
+                }
             }
         }
     }
+
 }
---
+++ app/src/main/java/com/ricky/jetpet/components/TopBar.kt
@@ -0,0 +1,83 @@
+package com.ricky.jetpet.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ricky.jetpet.R
+
+@Composable
+fun TopBar(onClick: () -> Unit) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = "Hey Lucky",
+                textAlign = TextAlign.Start,
+                style = MaterialTheme.typography.titleSmall,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(
+                text = "Find a new friend near to you to adopt",
+                textAlign = TextAlign.Start,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = 24.dp,
+                    end = 36.dp
+                )
+        ) {
+            PetSwitch {
+                onClick.invoke()
+            }
+        }
+    }
+}
+
+@Composable
+fun PetSwitch(onClick: () -> Unit) {
+    val icon =
+        if (isSystemInDarkTheme()) painterResource(id = R.drawable.ic_switch_on) else painterResource(
+            R.drawable.ic_switch_off
+        )
+
+    Icon(
+        painter = icon,
+        contentDescription = null,
+        modifier = Modifier
+            .size(24.dp)
+            .clickable { onClick.invoke() },
+        tint = MaterialTheme.colorScheme.onSurface
+    )
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun TopBarPreview() {
+    TopBar {
+
+    }
+}
